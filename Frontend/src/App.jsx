5375dc093fe48e866e2f2e980d304f1f78ac1949
Anu17shka <anushkagoyal42@gmail.com>
2024-06-18 20:24:57 +0530

    Navbar update

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Banner />
+    <Footer />
+
+    </>
+  )
+}
+
+export default App
